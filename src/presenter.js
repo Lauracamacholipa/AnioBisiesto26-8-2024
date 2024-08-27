ecce47ceee760aacc45e98db6aa09481b771c896
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-08-27 00:34:41 -0400

    Cambios en el presenter

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
+import esBisiesto from "./esBisiesto.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const numero = document.querySelector("#primer-numero"); 
+const form = document.querySelector("#aniobisiesto-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const numeroUsr = Number.parseInt(numero.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + esBisiesto(numeroUsr) + "</p>";
 });
